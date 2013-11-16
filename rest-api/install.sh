a9a916ea858d854a2bcea52cda327c36bcfde48a
Andrey Adamovich <andrey@aestasit.com>
2013-11-16 23:34:45 +0100

    updated javascript logic

--- rest-api/install.sh
+++ rest-api/install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install mongodb
 sudo apt-get install openjdk-7-jdk
 sudo mkdir -p /var/lib/tellme
+sudo mkdir -p /var/lib/tellme/storage
 sudo service ufw stop
 sudo apt-get install unzip
 sudo apt-get install nginx
--- rest-api/src/ratpack/public/js/general.js
+++ rest-api/src/ratpack/public/js/general.js
@@ -1,25 +1,61 @@
 jQuery(document).ready(function() { 
 
 	// Navigation
-	$('#answer a').click(function(){
+	$('#answer a').click( function() {
+			
 		var activeSlide = $(this).attr('data-slide');
 
-		if(activeSlide == 0) {
-			$(this).attr('data-slide', -1);
-		} else {
-			$(this).attr('data-slide', 0);
-		}
+		if (activeSlide == 0) {
+			
+			$(this).attr('data-slide', 1);					
+			$(this).text('Next Q');			
+			slideContent(activeSlide);			
+            
+		} else if (activeSlide == -1) {
 
-		$('#content').animate({
-		  left : (100*(activeSlide)) + "%"
-		}, 1000, function() {
-			if(activeSlide == -1) {				
-				drawChart(getData());
-			}
-		});
+			$(this).attr('data-slide', 0);
+			$(this).text('Back');
+			answerQuestion();			
+			slideContent(activeSlide);
+			
+		} else {
+			
+			$(this).attr('data-slide', 2);
+			$(this).attr('href', '/question/random');
+			return true;
+			
+		}
+		
+		return false;
+		
 	});
 
- });
+});
+
+function slideContent(activeSlide) {
+	$('#content').animate({
+	  left : (100 * (activeSlide)) + "%"
+	}, 1000, function() {
+		if (activeSlide == -1) {				
+			drawChart(getData());
+		}
+	});	
+}
+
+function answerQuestion() {
+  $.ajax({
+	  type: "POST",
+	  url: "/api/answers/new",
+	  data: 
+		'{ ' +
+		   '"question_id": "' + $('#question_id').val() + '",' +
+		   '"title": "' + $('.answers input').filter(':checked').val() + '"' + 
+		'}',
+	  dataType: "json"
+	});
+  $('#answer a').href = '/question/random'
+}
+
 
 // Draws a chart.
 function drawChart(data) {
