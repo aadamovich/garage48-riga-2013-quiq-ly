bad2aa6317cff7d6afeddbb337aaeeb1aa06afa6
Andrey Adamovich <andrey@aestasit.com>
2013-11-16 17:13:25 +0100

    fixed

--- rest-api/src/main/groovy/org/garage48/tellme/DataService.groovy
+++ rest-api/src/main/groovy/org/garage48/tellme/DataService.groovy
@@ -37,7 +37,9 @@ class DataService {
 			  ]
 			).results().each { answerStats ->
 			  question.answers.each { answer ->
-				  answer.value = 0
+				  if (answer.value == null) {
+					 answer.value = 0
+				  } 
 				  if (answerStats._id.title == answer.title) {
 					  answer.value = answerStats.count
 				  }
